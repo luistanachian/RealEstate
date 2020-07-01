a6541eb63501c16f296b1838724464efb0d0202f
luistanachian <53198490+luistanachian@users.noreply.github.com>
2020-06-30 21:47:53 -0300

    Direcciones Paises Controller y coleccion postman

--- RealEstate.API/Controllers/DireccionController.cs
+++ RealEstate.API/Controllers/DireccionController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RealEstate.Business.Interface;
+using RealEstate.Models.Entity;
+using System.Linq;
 
 namespace RealEstate.API.Controllers
 {
@@ -11,6 +13,57 @@ namespace RealEstate.API.Controllers
         public DireccionController(IDireccionPaisesBusiness direccionPaisesBusiness)
         {
             _direccionPaisesBusiness = direccionPaisesBusiness;
+        }
+
+        [Route("pais")]
+        [HttpGet]
+        public IActionResult PaisGetAll()
+        {
+            var paises = _direccionPaisesBusiness.GetAll();
+            if (paises != null && paises.Count() > 0)
+                return Ok(paises);
+
+            return NoContent();
+        }
+        [Route("pais/{id}")]
+        [HttpGet]
+        public IActionResult PaisGetById(string id)
+        {
+            var pais = _direccionPaisesBusiness.GetById(id);
+            if (pais != null)
+                return Ok(pais);
+
+            return NoContent();
+        }
+        [Route("pais")]
+        [HttpPut]
+        public IActionResult Insert(DireccionPaises pais)
+        {
+            var guardado = _direccionPaisesBusiness.Insert(pais);
+            if (guardado)
+                return Ok();
+
+            return BadRequest();
+        }
+        [Route("pais")]
+        [HttpPost]
+        public IActionResult Update(DireccionPaises pais)
+        {
+            var guardado = _direccionPaisesBusiness.Update(pais);
+            if (guardado)
+                return Ok();
+
+            return BadRequest();
+        }
+        [Route("pais/{id}")]
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            var guardado = _direccionPaisesBusiness.Delete(id);
+            if (guardado)
+                return Ok();
+
+            return BadRequest();
         }
     }
 }
